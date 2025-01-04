5e2804cbe10e8d8836f7050addfd5e6cd73d5f92
Laura Pais <pais.laura@gmail.com>
2025-01-04 18:32:22 +0000

    +

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,15 @@
 document.getElementById('menuOpen').addEventListener('click', function() {
     const menu = document.getElementById('menu');
     const button = document.getElementById('menuOpen');
+    const navSelect = document.querySelector('.navSelect'); // Select the navSelect element
 
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
         button.textContent = 'Menu';
+        navSelect.style.display = 'block'; // Show navSelect when menu is closed
     } else {
         menu.classList.add('open');
         button.textContent = 'Fechar';
+        navSelect.style.display = 'none'; // Hide navSelect when menu is open
     }
 });
