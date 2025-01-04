7f01095d55fc20ebb5fab2a1728408dcb29a98ff
Fábio Gouveia Silva <49789907+gouveiafabio98@users.noreply.github.com>
2025-01-04 23:29:24 +0000

    +

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,18 +1,3 @@
 document.getElementById('menuOpen').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    const button = document.getElementById('menuOpen');
-    const navSelect = document.querySelector('.navSelect');
-    const menuBackground = document.querySelector('.menuBackground');
-
-    if (menu.classList.contains('open')) {
-        menu.classList.remove('open');
-        menuBackground.classList.remove('open');
-        button.textContent = 'Menu';
-        navSelect.style.display = 'block';
-    } else {
-        menu.classList.add('open');
-        menuBackground.classList.add('open');
-        button.textContent = 'Fechar';
-        navSelect.style.display = 'none';
-    }
+    document.getElementById('menu').classList.toggle('open');
 });
